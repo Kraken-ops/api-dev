72a520fb4b56fc77833acab286fe8ccdace8084d
Priyanshu Dey <priyanshu.dey@globalids.com>
2023-10-05 20:11:15 +0530

    new change

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const app = express();
 const Product = require('./models/productModel')
 app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 //routes
 app.get("/", (req, res) => {
   res.send("Hello Node Api");
@@ -16,6 +17,34 @@ app.get('/product',async(req,res) => {
     try {
         const product = await Product.find({});
         res.status(200).json(product);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message : error.message})
+    }
+})
+
+app.get('/product/:id', async(req,res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message : error.message})
+    }
+})
+
+app.put('/product/:id', async(req,res) => {
+    try {
+        const {id} =req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body)
+        //res.status(200).json(product);
+        if(!product)
+        {
+            return res.status(404).json({message : `Could not find any product with id ${id}`})
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
     } catch (error) {
         console.log(error.message);
         res.status(500).json({message : error.message})
